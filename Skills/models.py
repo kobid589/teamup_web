879032feda21173ac9cc25436cd48997f9c24e7f
kiranneupane11 <kiran10neupane@gmail.com>
2023-03-14 18:17:45 +0545

    api1

--- Skills/models.py
+++ Skills/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class Skills(models.Model):
     skill_name = models.CharField(max_length=255)
-    skill_level= models.CharField(max_length=255)
+    skill_level = models.CharField(max_length=255)
--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,14 @@
-from django.http import HttpResponse, JsonResponse
+
 from django.urls import path
-
-from apps.Expertise.models import Expertise
-
-
-def list_room(request):
-    expertise_list = Expertise.objects.all()
-    payload = []
-    for expertise in expertise_list:
-        json_data = {
-            'id': expertise.id,
-            'name': expertise.name,
-        }
-        payload.append(json_data)
-    return HttpResponse(JsonResponse({'payload': payload}))
+from api.views import list_room, list_of_organization,\
+    list_of_highlights, list_of_awards, list_of_skills,list_of_experiences
 
 
 urlpatterns = [
     path('rooms/list', list_room, name='rooms-list'),
+    path('organisation', list_of_organization, name='org_list'),
+    path('highlights', list_of_highlights, name='highlights'),
+    path('awards', list_of_awards, name='awards'),
+    path('skills', list_of_skills, name='skills'),
+    path('experiences', list_of_experiences, name='experiences')
 ]
