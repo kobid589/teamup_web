d244b214660591240cdfd4b54aac64873b09b24f
MANISH <gyawalimanish12345@gmail.com>
2023-03-14 14:45:40 +0545

    api

--- login/models.py
+++ login/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class login(models.Model):
-    username_email=models.CharField(verbose_name=('Username/Email'), max_length=255)
+    username_email=models.CharField(verbose_name=('Username/Email'), max_length=255,null=True, blank=True)
     password = models.CharField(max_length=255)
 
